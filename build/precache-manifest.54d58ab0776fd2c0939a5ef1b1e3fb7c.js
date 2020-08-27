5acc1d151863b658273dc0ed5e56557d434b9378
Abu Zahrah <abuzahrah75@gmail.com>
2020-08-27 11:27:21 +0800

    first build for testing

---
+++ build/precache-manifest.54d58ab0776fd2c0939a5ef1b1e3fb7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bf8407e4cb82d06ef1c8ee09c46be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886865f1414e927b5f08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23735bb8b300fef6645e",
+    "url": "/static/js/2.a9456eb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9456eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886865f1414e927b5f08",
+    "url": "/static/js/main.086c36ae.chunk.js"
+  },
+  {
+    "revision": "55efe3ed243828dcf931",
+    "url": "/static/js/runtime-main.a8d89da3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
